b90445cf2fe485c494ca6d508fc967867bb34cc7
RatmirShchadrynski <60134140+WhiteW1ine@users.noreply.github.com>
2022-12-16 20:24:56 +0200

    Update
    Update styles and made a finish box

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,6 @@ const startingApiURL=  'https://courselab.lnu.se/quiz/question/1'
 
 const timer = new Timer(17);
 const question_text = document.getElementById('question')
-const first_option_answer = document.getElementById('a_text')
-const second_option_answer = document.getElementById('b_text')
-const third_option_answer = document.getElementById('c_text')
-const fourth_option_answer = document.getElementById('d_text')
 const submit = document.getElementById('submit')
 const start_button = document.getElementById('start')
 const submit_button = document.getElementById('submit')
@@ -19,6 +15,7 @@ const quiz_box = document.getElementById('quiz')
 const registration_box = document.getElementById('registration')
 const input_name_form = document.getElementById('nickName')
 const localStorage = window.localStorage;
+const resulsts_box = document.getElementById('results')
 
 
 
@@ -27,11 +24,11 @@ start_button.addEventListener('click', () => {
     quiz_box.classList.remove('hidden')
     registration_box.classList.add('hidden')
     startGame()
-})
+}, {once: true})
 
 submit_button.addEventListener('click', () => {
     startTimer()
-})
+}, {once: true})
 
 
 function startGame() {
@@ -48,32 +45,54 @@ async function loadQuestion(url) {
     question_text.innerHTML = data.question
     if (data.hasOwnProperty('alternatives')){
         multiAnswers(data)
+        console.log('multi')
     }
     else {
         singleAnswer(data)
+        console.log('single')
     }
        
 }
 
 
 function multiAnswers(data) {
+    
     let alternatives = data.alternatives
     let form = document.createElement('form')
-    
+    form.classList.add('form')
     let targetElement = document.getElementById("submit");
     let parent = targetElement.parentNode
     parent.insertBefore(form, targetElement)
+
     for(let alternative in alternatives){
-        addRadioButton(form, alternative, alternative.key, alternative.value)
+        addRadioButton(form, alternative, alternatives[alternative])
     }
+
+
+    submit_button.addEventListener('click', async function() {
+        let selectedRadio = form.querySelector('input[name="buttonsGroup"]:checked')
+        let postData = await Request.POST(selectedRadio.value, data.nextURL)
+        if (!postData.hasOwnProperty('nextURL')){
+            finishGame()
+        }
+        loadQuestion(postData.nextURL)
+        form.remove()
+    },{once: true})
+
+
 }
 
-function addRadioButton(form, name, value) {
+function addRadioButton(form, value, labelText) {
     let radioButton = document.createElement('input')
-    radioButton.type = "radio";
-    radioButton.name = name;
+    radioButton.type = 'radio';
+    radioButton.name = 'buttonsGroup';
     radioButton.value = value;
-    form.appendChild(radioButton)
+
+    let label = document.createElement('label')
+    label.appendChild(radioButton);
+    label.appendChild(document.createTextNode(labelText))
+
+    form.appendChild(label)
 }
 
 
@@ -91,12 +110,20 @@ function singleAnswer(data) {
 
     
     submit_button.addEventListener('click', async function() {
-        console.log(input.value)
         let postData = await Request.POST(input.value, data.nextURL)
+        if (!postData.hasOwnProperty('nextURL')){
+            finishGame()
+        }
         loadQuestion(postData.nextURL)
         input.remove()
-    })
-
-
+    }, {once:true})
 
 }
+
+function finishGame() {
+    quiz_box.classList.add('hidden')
+    resulsts_box.classList.remove('hidden')
+    
+}
+
+
